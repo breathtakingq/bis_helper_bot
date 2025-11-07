89fcc4d59c0c528833eab276f991b0cccc4e8555
breathtaking <160609127+breathtakingq@users.noreply.github.com>
2025-11-07 23:17:08 +0300

    Update Main.java

--- src/main/java/ru/mybot/Main.java
+++ src/main/java/ru/mybot/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         MongoLogger.init("mongodb://localhost:27017", "mybot_mongo", "logs");
         try {
-            String TOKEN = "8295626092:AAEWkGc0V8zd1TJWnD7jKIh3v7jVjghP3YI";
+            String TOKEN = "YOUR_BOT_TOKEN";
 
             TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
             api.registerBot(new Bot(TOKEN));
